d757c692680e462abf0ac095888da4fb8e51001d
MariiaZhk <maria8zhuk@gmail.com>
2023-12-14 01:20:08 +0100

    initial commit

--- src/services/api.js
+++ src/services/api.js
@@ -7,3 +7,36 @@ import axios from 'axios';
 // /movies/get-movie-details запит повної інформації про фільм для сторінки кінофільму.
 // /movies/get-movie-credits запит інформації про акторський склад для сторінки кінофільму.
 // /movies/get-movie-reviews запит оглядів для сторінки кінофільму.
+
+const KEY = 'def629929c99c65aed82e08d2ebce959';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+export async function fetchTrendingMovies(day) {
+  const response = await axios.get('/trending/movie/', {
+    params: {
+      key: KEY,
+      time_window: day,
+    },
+  });
+  return response.data;
+}
+
+export async function fetchSearchMovies(query) {
+  const response = await axios.get('/search/movie', {
+    params: {
+      key: KEY,
+      query,
+    },
+  });
+  return response.data;
+}
+export async function fetchMoviesDetails(query, movie_id) {
+  const response = await axios.get('/search/movie', {
+    params: {
+      key: KEY,
+      query,
+      movie_id,
+    },
+  });
+  return response.data;
+}
